5f14d0f22e0cd3b0af7b7c3cc7cb98304b796c53
hamishthepiper <31934923+hamishthepiper@users.noreply.github.com>
2017-09-30 09:51:19 -0400

    Critical damage complete

--- Main.py
+++ Main.py
@@ -7,12 +7,33 @@ atk = random.randrange(0, 20)
 if atk == 0:
     print("Critical fail!")
 
+elif atk == 20:
+    pwning = True
+    print("Natty 20! You strike your enemy for critical damage.")
+    while pwning:
+        num_dice = int(input("How many dice?:")) * 2
+        max_side = int(input("How many sides?:"))
+        atk_mod = int(input("Attack modifier?:"))
+        die_roll = 0
+        i = 0
+        dmg = 0
+
+        while i < num_dice:
+            die_roll = random.randrange(1, max_side)
+            print("Dice #" + str(i + 1) + " rolled " + str(die_roll) + " damage.")
+            dmg = dmg + die_roll
+            i += 1
+        dmg = dmg + atk_mod
+
+        print("You attacked for", dmg, "damage.")
+        pwning = False
+
 else:
     atk = int(input("Enter attack modifier:")) + atk
 
-    if atk > enemy_ac:
+    if atk >= enemy_ac < 20:
         winning = True
-        print("You strike your enemy!")
+        print("Your attack succeeds.")
         while winning:
             num_dice = int(input("How many dice?:"))
             max_side = int(input("How many sides?:"))
@@ -23,12 +44,13 @@ else:
 
             while i < num_dice:
                 die_roll = random.randrange(1, max_side)
-                print(die_roll)
+                print("Dice #" + str(i + 1) + " rolled " + str(die_roll) + " damage.")
                 dmg = dmg + die_roll
                 i += 1
             dmg = dmg + atk_mod
 
             print("You attacked for", dmg, "damage.")
             winning = False
+
     else:
         print("Your attack fails to penetrate your enemies defenses!")
